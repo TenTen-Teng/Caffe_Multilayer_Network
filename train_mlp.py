436d29fcbf221ab89dbdf0624da5dc519de59e1a
Ubuntu <ubuntu@ip-172-31-21-38.ec2.internal>
2018-04-10 21:37:00 +0000

    full program

---
+++ train_mlp.py
@@ -0,0 +1,39 @@
+
+import os
+import caffe
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy
+import time
+
+caffe.set_mode_gpu()
+
+solver = caffe.SGDSolver('mlp_solver.prototxt')
+
+niter =20000
+test_interval = 100
+train_loss = np.zeros(niter)
+test_acc = np.zeros(int(np.ceil(niter / test_interval)))
+
+start = time.time()
+# the main solver loop
+for it in range(niter):
+    solver.step(1)  # SGD by Caffe
+
+    # store the train loss
+    train_loss[it] = solver.net.blobs['loss'].data
+    solver.test_nets[0].forward(start='input1')
+
+    if it % test_interval == 0:
+        acc=solver.test_nets[0].blobs['accuracy'].data
+        print 'Iteration', it, 'testing...','accuracy:',acc
+        test_acc[it // test_interval] = acc
+
+end = time.time()
+print("time: ", end-start)
+
+
+
+
+
+
